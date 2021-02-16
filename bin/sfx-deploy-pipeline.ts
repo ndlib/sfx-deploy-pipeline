80fe0be6c461c263ed6d0cebc83cb70f5abd7bf3
Justin Rittenhouse <jrittenh@nd.edu>
2021-02-16 16:49:08 -0500

    Check in first-pass ts code

--- bin/sfx-deploy-pipeline.ts
+++ bin/sfx-deploy-pipeline.ts
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { SfxDeployPipelineStack } from '../lib/sfx-deploy-pipeline-stack';
+import 'source-map-support/register'
+import { App, Aspects } from '@aws-cdk/core'
+import { StackTags } from '@ndlib/ndlib-cdk'
+import { SfxDeployPipelineStack } from '../src/sfx-deploy-pipeline-stack'
 
-const app = new cdk.App();
-new SfxDeployPipelineStack(app, 'SfxDeployPipelineStack');
+const app = new App()
+const oauthTokenPath = app.node.tryGetContext("oauthTokenPath")
+const sourceRepoOwner = app.node.tryGetContext("sourceRepoOwner")
+const sourceRepository = app.node.tryGetContext("sourceRepository")
+const sourceBranch = app.node.tryGetContext("sourceBranch")
+const networkStackName = app.node.tryGetContext("networkStackName")
+const owner = app.node.tryGetContext("owner")
+const contact = app.node.tryGetContext("contact")
+const notifyStackName = app.node.tryGetContext("notifyStackName")
+
+const service = new SfxDeployPipelineStack(app, 'SfxDeploy', {
+    oauthTokenPath,
+    sourceRepoOwner,
+    sourceRepository,
+    sourceBranch,
+    networkStackName,
+    owner,
+    contact,
+    notifyStackName,
+})
+
+// Apply tags to all stacks
+Aspects.of(app).add(new StackTags())
--- src/sfx-deploy-pipeline-stack.ts
+++ src/sfx-deploy-pipeline-stack.ts
@@ -1,9 +1,254 @@
-import * as cdk from '@aws-cdk/core';
+import codebuild = require('@aws-cdk/aws-codebuild')
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline'
+import { GitHubSourceAction, CodeBuildAction, ManualApprovalAction } from '@aws-cdk/aws-codepipeline-actions'
+import { Vpc, SecurityGroup } from '@aws-cdk/aws-ec2'
+import { PolicyStatement } from '@aws-cdk/aws-iam'
+import { Topic } from '@aws-cdk/aws-sns'
+import { Construct, Fn, SecretValue, Stack, StackProps } from '@aws-cdk/core'
+import { ArtifactBucket, SlackApproval } from '@ndlib/ndlib-cdk'
 
-export class SfxDeployPipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export interface SfxPipelineStackProps extends StackProps {
+  readonly oauthTokenPath: string
+  readonly sourceRepoOwner: string
+  readonly sourceRepository: string
+  readonly sourceBranch: string
+  readonly owner: string
+  readonly contact: string
+  readonly networkStackName: string
+  readonly notifyStackName: string
+}
 
-    // The code that defines your stack goes here
+
+export class SfxDeployPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props: SfxPipelineStackProps) {
+    super(scope, id, props)
+
+    const owner = props.owner || `see stack: ${this.stackName}`
+    const contact = props.contact || `see stack: ${this.stackName}`
+
+    // S3 bucket for storing artifacts
+    const artifactBucket = new ArtifactBucket(this, 'ArtifactBucket', {})
+
+    // SNS Topic for approvals
+    const approvalTopic = new Topic(this, 'ApprovalTopic')
+
+    // Add VPC information as this needs to run out of the peered network
+    const vpcId = Fn.importValue(`${props.networkStackName}:VPCID`)
+    const vpc = Vpc.fromVpcAttributes(this, 'ImportedVPC', {
+      vpcId,
+      availabilityZones: [
+        // This technically doesn't matter in this context, since none of the resources in this app
+        // require AZ for their cloud formations, only subnets. But in the interest of not creating
+        // problems for future things that use this IVpc object, I'm recreating how AZs were defined
+        // for the subnets in the network stack. In those stacks, we aren't exporting the AZs for
+        // Subnet1|2 so this must match the way the subnets were created.
+        Fn.select(0, Fn.getAzs()),
+        Fn.select(1, Fn.getAzs()),
+      ],
+      publicSubnetIds: [
+        Fn.importValue(`${props.networkStackName}:PublicSubnet1ID`),
+        Fn.importValue(`${props.networkStackName}:PublicSubnet2ID`),
+      ],
+      privateSubnetIds: [
+        Fn.importValue(`${props.networkStackName}:PrivateSubnet1ID`),
+        Fn.importValue(`${props.networkStackName}:PrivateSubnet2ID`),
+      ],
+    })
+
+    const sfxDeploySecurityGroup = new SecurityGroup(this, 'SfxDeploySecurityGroup', { vpc, allowAllOutbound: true })
+
+    // Retrieve source
+    const sourceArtifact = new Artifact('SourceCode')
+    const sourceAction = new GitHubSourceAction({
+      actionName: 'SourceCode',
+      branch: props.sourceBranch,
+      oauthToken: SecretValue.secretsManager(props.oauthTokenPath, { jsonField: 'oauth' }),
+      output: sourceArtifact,
+      owner: props.sourceRepoOwner,
+      repo: props.sourceRepository,
+    })
+
+    const buildHelper = (namespace: string) => {
+      return new codebuild.PipelineProject(this, `SfxDeploy_${props.sourceRepository}_${namespace}`, {
+        vpc: vpc,
+        securityGroups: [
+          sfxDeploySecurityGroup,
+        ],
+        buildSpec: codebuild.BuildSpec.fromObject({
+          env: {
+            shell: 'bash',
+          },
+          phases: {
+            build: {
+              commands: [
+                'apt-get update -qq',
+                'apt-get install -y rsync ftp sshpass netcat',
+              ]
+            }
+          },
+          version: '0.2',
+        }),
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+          computeType: codebuild.ComputeType.SMALL,
+        },
+        environmentVariables: {
+          REMOTE_HOST: {
+            value: `/all/sfx/ftp/hostname`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+          REMOTE_USER: {
+            value: `/all/sfx/ftp/${namespace}/username`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+          REMOTE_PASS: {
+            value: `/all/sfx/ftp/${namespace}/password`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+          REMOTE_PATH: {
+            value: `/all/sfx/ftp/${namespace}/path`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+        }
+      })
+    }
+
+    const smokeTestHelper = (namespace: string) => {
+      return new codebuild.PipelineProject(this, `${namespace}-SmokeTests`, {
+        buildSpec: codebuild.BuildSpec.fromObject({
+          phases: {
+            build: {
+              commands: [
+                `newman run spec/SFX.postman_collection.json --folder Smoke --env-var hostname=$HOSTNAME --env-var env=$REMOTE_PATH`,
+              ],
+            },
+          },
+          version: '0.2',
+        }),
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.fromDockerRegistry('postman/newman'),
+        },
+        environmentVariables: {
+          REMOTE_PATH: {
+            value: `/all/sfx/ftp/${namespace}/path`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+          HOSTNAME: {
+            value: `/all/sfx/ftp/hostname`,
+            type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+          },
+        }
+      })
+    }
+
+    const testBuild = buildHelper(`test`)
+    testBuild.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameters',
+      ],
+      resources: [
+        Fn.sub('arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/all/sfx/ftp/*'),
+      ],
+    }))
+
+    const prodBuild = buildHelper(`prod`)
+    prodBuild.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameters',
+      ],
+      resources: [
+        Fn.sub('arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/all/sfx/ftp/*'),
+      ],
+    }))
+
+    const testSmokeTest = smokeTestHelper(`test`)
+    testSmokeTest.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameters',
+      ],
+      resources: [
+        Fn.sub('arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/all/sfx/ftp/*'),
+      ],
+    }))
+
+    const prodSmokeTest = smokeTestHelper(`prod`)
+    testSmokeTest.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameters',
+      ],
+      resources: [
+        Fn.sub('arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/all/sfx/ftp/*'),
+      ],
+    }))
+
+    const slackApproval = new SlackApproval(this, 'SlackApproval', {
+      approvalTopic,
+      notifyStackName: props.notifyStackName,
+    })
+
+    const testSfxDeployAction= new CodeBuildAction({
+      input: sourceArtifact,
+      project: testBuild,
+      actionName: 'TestBuildAndDeploy',
+      runOrder: 1,
+    })
+
+    const prodSfxDeployAction = new CodeBuildAction({
+      input: sourceArtifact,
+      project: prodBuild,
+      actionName: 'ProdBuildAndDeploy',
+      runOrder: 1,
+    })
+
+    const testSmokeTestsAction = new CodeBuildAction({
+      input: sourceArtifact,
+      project: testSmokeTest,
+      actionName: 'SmokeTests',
+      runOrder: 98,
+      environmentVariables: {
+        REMOTE_PATH: {
+          value: `/all/sfx/ftp/test/smokepath`,
+          type: codebuild.BuildEnvironmentVariableType.PARAMETER_STORE,
+        },
+      }
+    })
+
+    const prodSmokeTestsAction = new CodeBuildAction({
+      input: sourceArtifact,
+      project: prodSmokeTest,
+      actionName: 'SmokeTests',
+      runOrder: 99,
+      environmentVariables: {
+        REMOTE_PATH: {
+          value: ``,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+        },
+      }
+    })
+
+    const slackApprovalAction = new ManualApprovalAction({
+      actionName: 'ManualApproval',
+      additionalInformation: `A new version of https://github.com/ndlib/sfx has been deployed to https://>SFX_ADDRESS_HERE< and is awaiting your approval. If you approve these changes, they will be deployed to https://>SFX_ADDRESS_HERE<.`,
+      notificationTopic: approvalTopic,
+      runOrder: 99,
+    })
+
+    const pipeline = new Pipeline(this, 'SfxDeployPipeline', {
+      artifactBucket,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'DeployTest',
+          actions: [testSfxDeployAction, testSmokeTestsAction, slackApprovalAction],
+        },
+        {
+          stageName: 'DeployProd',
+          actions: [prodSfxDeployAction, prodSmokeTestsAction],
+        },
+      ]
+    })
   }
 }
